0b54b2a05930a28b59c5cb2cc8162884ab5a296b
刘六乾 <liuliuqian@yanxiu.com>
2019-11-11 17:32:19 +0800

    fix

---
+++ ShoppingAPP/lattecore/src/main/java/com/example/lattecore/ui/loader/LoaderCreator.java
@@ -0,0 +1,12 @@
+package com.example.lattecore.ui.loader;
+
+import com.wang.avi.Indicator;
+
+import java.util.WeakHashMap;
+
+final public class LoaderCreator {
+
+    private static final WeakHashMap<String, Indicator> LOADING_MAP = new WeakHashMap<>();
+
+
+}
